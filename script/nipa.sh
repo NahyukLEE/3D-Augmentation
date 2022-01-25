69a97285e34eb714365330483cc57a6edaad50da
Nahyuk Lee <51054576+NahyukLEE@users.noreply.github.com>
2022-01-25 22:26:03 +0900

    Update nipa.sh

--- script/nipa.sh
+++ script/nipa.sh
@@ -5,9 +5,9 @@ read -p "Type which server do you want to connect(1 or 2): " server
 if [ $server == 1 -o $server == 2 ] ; then
 	echo "Connect to server $server..."
 	if [ $server == 1 ] ; then
-		ssh ubuntu@211.188.81.143 -p 16022
+		ssh nahyuklee@211.188.81.143 -p 16022
 	elif [ $server == 2 ] ; then
-		ssh ubuntu@000.000.000.000 -p 16022
+		ssh nahyuklee@211.188.81.168 -p 16022
 	fi
 else
 	echo "[Unexpected Input] Type 1 or 2"
