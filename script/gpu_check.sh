f2d5bb1329fb06a659d0d9ff04a0a87a95a20ff3
Nahyuk Lee <51054576+NahyukLEE@users.noreply.github.com>
2022-01-26 16:50:10 +0900

    Create gpu_check.sh

---
+++ script/gpu_check.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+// Please use command 'source gpu_check.sh'
+
+echo "-------------------------------------"
+echo "Check CUDA Version"
+nvcc -V
+
+echo "-------------------------------------"
+echo "Check cuDNN Version"
+
+CUDNNH="/usr/local/cuda/include/cudnn.h"
+CUDNNVH="/usr/local/cuda/include/cudnn_version.h"
+
+if [ -e $CUDNNH  ]; then
+        cat /usr/local/cuda/include/cudnn.h | grep CUDNN_MAJOR -A 2
+elif [ -e $CUDNNVH ]; then
+        cat /usr/local/cuda/include/cudnn_version.h | grep CUDNN_MAJOR -A 2
+else
+        echo "No cuDNN installation"
+fi;
+
+echo "-------------------------------------"
+echo "Check done!"
